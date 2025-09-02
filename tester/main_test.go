083199ff8c7bcf3785d89d82eb314418c2b0354a
Kevin Kruger <kkruger@launchdarkly.com>
2025-09-02 09:57:19 -0400

    add a new table and query

--- tester/main_test.go
+++ tester/main_test.go
@@ -22,6 +22,13 @@ func TestMain(t *testing.T) {
 		t.Fatalf("Failed to query test database: %v", err)
 	}
 	defer rows.Close()
+
+	sql = "SELECT * FROM test_schema.testtable2 where bunch_of_nested_json->>'test'->>'test2' = 'test3'"
+	rows, err = db.Query(sql)
+	if err != nil {
+		t.Fatalf("Failed to query test database: %v", err)
+	}
+	defer rows.Close()
 }
 
 func getTestDbConnection() (*sql.DB, error) {
